d48093ec0727ba00694debb0162e559635729bf4
Cuong Nguyen <cuongnguyencao2906@gmail.com>
2021-04-13 04:34:59 +0700

    Fix V1Client for new proto file structure

--- python/gubernator/__init__.py
+++ python/gubernator/__init__.py
@@ -1,10 +1,9 @@
 # This code is py3.7 and py2.7 compatible
-
-import gubernator.ratelimit_pb2_grpc as pb_grpc
-from datetime import datetime
-
 import time
 import grpc
+from datetime import datetime
+from gubernator.gubernator_pb2_grpc import V1Stub
+
 
 MILLISECOND = 1
 SECOND = MILLISECOND * 1000
@@ -18,4 +17,4 @@ def sleep_until_reset(reset_time):
 
 def V1Client(endpoint='127.0.0.1:9090'):
     channel = grpc.insecure_channel(endpoint)
-    return pb_grpc.RateLimitServiceV1Stub(channel)
+    return V1Stub(channel)
